0d88e68db30fb90cd8c964ce067ff6b636165ec9
ap-cloud-bit <la.mist96@gmail.com>
2025-09-19 23:46:36 +0500

    ingest.py

--- ingest.py
+++ ingest.py
@@ -1,20 +1,12 @@
 from pinecone import Pinecone
 import streamlit as st
 
-PINECONE_API_KEY = st.secrets["PINECONE_API_KEY"]
+# Load API keys & index name
+PINECONE_API_KEY = st.secrets["pcsk_4VU2a9_MbzptHb81BUbcEoEu8hvL3hGSZjZHThq5JLqV5JkDZ1LYUcNLEQqqRtNVBvm8XQ"]
 INDEX_NAME = st.secrets.get("PINECONE_INDEX_NAME", "research-index")
 
+# Init Pinecone
 pc = Pinecone(api_key=PINECONE_API_KEY)
 
-# Check if index already exists
-existing_indexes = pc.list_indexes().names()
-
-if INDEX_NAME not in existing_indexes:
-    pc.create_index(
-        name=INDEX_NAME,
-        dimension=1536,
-        metric="cosine"
-    )
-
-# Connect to index
+# Connect to your existing index
 index = pc.Index(INDEX_NAME)
