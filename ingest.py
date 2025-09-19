8b3c08f3bcba026631ec8726e407aff6e1bf24a3
ap-cloud-bit <la.mist96@gmail.com>
2025-09-20 00:03:53 +0500

    ingest.py

--- ingest.py
+++ ingest.py
@@ -1,8 +1,5 @@
 import streamlit as st
-from pinecone import Pinecone
 
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 PINECONE_API_KEY = st.secrets["PINECONE_API_KEY"]
 INDEX_NAME = st.secrets["PINECONE_INDEX_NAME"]
-
-pc = Pinecone(api_key=PINECONE_API_KEY)
-index = pc.Index(INDEX_NAME)
